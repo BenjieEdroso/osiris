fa12fae026447c335c31ab34c67f76742eb49e04
BenjieEdroso <ben09099@gmail.com>
2022-08-04 19:56:51 +0800

    added mobile nav, styled mobile nav, added close and open function

---
+++ index.js
@@ -0,0 +1,18 @@
+import mobileNav from "./mobileNav.js";
+let navButton = document.querySelector(".menu-btn");
+
+navButton.addEventListener("click", () => {
+  let app = document.querySelector(".app");
+  app.insertAdjacentHTML("beforeend", mobileNav);
+  let navTransition = setTimeout(() => {
+    app.lastElementChild.style.transform = "translate(0%)";
+  }, 1);
+  //close button
+  app.lastElementChild.children[0].addEventListener("click", () => {
+    app.lastElementChild.style.transform = "translate(100%)";
+    app.lastElementChild.addEventListener("transitionend", () => {
+      clearInterval(navTransition);
+      app.lastElementChild.remove();
+    });
+  });
+});
---
+++ mobileNav.js
@@ -0,0 +1,8 @@
+const mobileNav = `<div class="mobile-nav"><button class="mobile-close-button"><span class="material-symbols-outlined close-icon"> close </span>
+</button><nav><ul class="mobile-nav-links"><li><a href="#" class="link">Home</a></li>
+<li><a href="#" class="link">Collection</a></li>
+<li><a href="#" class="link">About</a></li>
+<li><a href="#" class="link">Contact</a></li>
+</ul></nav></div>`;
+
+export default mobileNav;
