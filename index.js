13e2013b1c6837961c1a56f86211de0dfdb1794e
BenjieEdroso <ben09099@gmail.com>
2022-08-05 18:22:17 +0800

    latest

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ let navButton = document.querySelector(".menu-btn");
 
 navButton.addEventListener("click", () => {
   let app = document.querySelector(".app");
+  app.classList.add("overflowHide");
   app.insertAdjacentHTML("beforeend", mobileNav);
   let navTransition = setTimeout(() => {
     app.lastElementChild.style.transform = "translate(0%)";
   }, 1);
   //close button
   app.lastElementChild.children[0].addEventListener("click", () => {
+    app.classList.remove("overflowHide");
     app.lastElementChild.style.transform = "translate(100%)";
     app.lastElementChild.addEventListener("transitionend", () => {
       clearInterval(navTransition);
